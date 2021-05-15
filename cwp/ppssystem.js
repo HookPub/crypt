ff23318042acf2258a2fb7f68c9c27c513e9450f
HookPub <hookpublishing@gmail.com>
2021-05-15 14:06:18 -0500

    format touch up and save system tweak

--- cwp/ppssystem.js
+++ cwp/ppssystem.js
@@ -5,10 +5,10 @@ let saveFile = () => {
 
     let data =
 
-        'Message: ' + msg.value;
+        msg.value;
 
     const textToBLOB = new Blob([data], { type: 'text/plain' });
-    const sFileName = 'formData.txt';	   // The file to save the data.
+    const sFileName = 'Document.txt';	   // The file to save the data.
 
     let newLink = document.createElement("a");
     newLink.download = sFileName;
