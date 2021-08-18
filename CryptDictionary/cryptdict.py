d25eb375efd3c092caf52cf939ad3018be91caa6
HookPub <hookpublishing@gmail.com>
2021-08-18 18:12:12 -0500

    touch up push

---
+++ CryptDictionary/cryptdict.py
@@ -0,0 +1,17 @@
+toc = {
+    "Genuity",
+    "Polymathic",
+
+}
+
+Genuity = {
+"The practice of genuineness"
+}
+
+Polymathic= {
+ "Displaying traits of a polymath/being multi-skilled"
+}
+
+# Legend:
+# Navigation Commands_
+# toc= Table of contents
