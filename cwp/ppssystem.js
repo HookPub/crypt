4e49076adbc1c3655c09ccf47da6b6240afc0391
HookPub <hookpublishing@gmail.com>
2021-10-08 12:39:03 -0500

    writer save system fix

--- cwp/ppssystem.js
+++ cwp/ppssystem.js
@@ -1,31 +1,3 @@
-let saveFile = () => {
-
-
-    const msg = document.getElementById('inputTextToSave');
-
-    let data =
-
-        msg.value;
-
-    const textToBLOB = new Blob([data], { type: 'text/plain' });
-    const sFileName = 'Document.html';	   // The file to save the data.
-
-    let newLink = document.createElement("a");
-    newLink.download = sFileName;
-
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    }
-    else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
-    }
-
-
-  newLink.click();
-}
-
 function saveTextAsFile()
 {
     var textToSave = document.getElementById("inputTextToSave").value;
@@ -47,4 +19,17 @@ function saveTextAsFile()
 function destroyClickedElement(event)
 {
     document.body.removeChild(event.target);
+}
+
+function loadFileAsText()
+{
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+    var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent)
+    {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        document.getElementById("inputTextToSave").value = textFromFileLoaded;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
 }
