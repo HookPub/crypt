070618204fc1c797b428d84283f11c03ce0914d0
HookPub <hookpublishing@gmail.com>
2022-01-21 11:12:11 -0600

    more greeter tweaks

--- greeter.js
+++ greeter.js
@@ -3,14 +3,11 @@ let greeting;
 if (time < 11) {
   greeting = "Good morning, Western Man.";
 }
-else if (time > 11) {
+else if (time > 11 < 15) {
   greeting = "Good afternoon, Western Man.";
 }
-else if (time < 15) {
-  greeting = "Good Evening, Western Man.";
-}
 else {
-  greeting = "Hello, Western Man.";
+  greeting = "Good Evening, Western Man.";
 }
 document.getElementById("greeting").innerHTML = greeting;
 
