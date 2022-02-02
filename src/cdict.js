188b29a57e4c2d91b5f1cf3cbaa0ca74d0ffa671
HookPub <hookpublishing@gmail.com>
2022-02-02 15:46:13 -0600

    cdict fix

--- src/cdict.js
+++ src/cdict.js
@@ -1,2 +1,3 @@
 var Genuity = "Genuity: The practice of genuineness."
 var Polymathic = "Polymathic: Displaying traits of a polymath/being multi-skilled."
+var Skro = "Skro: A reference to a term used in Idiocracy as a cannibalization of sup and bro, <br> with its usage now being to address an idiot in a dubious and pejorative way -> 'Whats up skro?'"
