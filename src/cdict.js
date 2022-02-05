7e0134847813250c2e473e7feee6fb47028313c1
HookPub <hookpublishing@gmail.com>
2022-02-04 22:56:03 -0600

    dict updates

--- src/cdict.js
+++ src/cdict.js
@@ -1,3 +1,4 @@
 var Genuity = "Genuity: The practice of genuineness."
 var Polymathic = "Polymathic: Displaying traits of a polymath/being multi-skilled."
-var Skro = "Skro: A reference to a term used in Idiocracy as a cannibalization of sup and bro, <br> with its usage now being to address an idiot in a dubious and pejorative way -> 'Whats up skro?'"
+var Skro = "Skro: A reference to a term used in Idiocracy as a portmanteau of sup and bro, <br> with its usage now being to address an idiot in a dubious and pejorative way -> 'Whats up skro?'"
+var Niglish = "Niglish: Ebonic English, usually riddled with subpar grammar and expletives "
