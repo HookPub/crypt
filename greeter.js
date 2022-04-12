aac1e136f4cc4fa0a27c2c723f1db85fc8afbb11
HookPub <hookpublishing@gmail.com>
2022-04-12 09:58:03 -0500

    big changes.

--- greeter.js
+++ greeter.js
@@ -1,13 +1,13 @@
 const time = new Date().getHours();
 let greeting;
-if (time < 11) {
+if (time < 12) {
   greeting = "Good morning, Western Man.";
 }
-else if (time > 11 ) {
+else if (time < 17) {
   greeting = "Good afternoon, Western Man.";
 }
-else if (time > 15) {
-  greeting = "Good Evening, Western Man.";
+else {
+ wgreeting = "Good evening, Western Man.";
 }
 
 document.getElementById("greeting").innerHTML = greeting;
